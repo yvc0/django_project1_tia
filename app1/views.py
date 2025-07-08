d1e8698d208ad680c38634d61c5ef87ab28e4f37
yvc0 <88483007+yvc0@users.noreply.github.com>
2025-07-08 08:12:05 +0530

    Add files via upload

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def show_output(request):
+    # Simple Python logic: add two numbers
+    a = 10
+    b = 5
+    result = a + b
+    return HttpResponse(f"The sum of {a} and {b} is {result}")
